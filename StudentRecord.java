977f09e4b49c7360a42825ab99e174213b2ae820
smithbot <dansmithbot@gmail.com>
2023-08-31 14:51:25 -0400

    Refactor Student Record and Demo Classes
    Refactor the StudentRecord and StudentRecordDemo classes to improve code organization, readability, and adherence to coding standards. Specifically:
    
    - In the StudentRecord class, update the class description to accurately reflect its purpose.
    - Remove unnecessary comments that don't provide additional clarity.
    - Simplify and clarify the StudentRecord constructor by initializing fields directly.
    - In the StudentRecord class, override the toString() method to provide a clear string representation.
    - In the StudentRecordDemo class, update the class description to reflect its purpose.
    - Update comments in the StudentRecordDemo class for better explanation.
    - Organize code in the StudentRecordDemo class for readability.
    - Update the creation and display of student records for consistency and clarity.
    - Provide an updated date in the StudentRecordDemo class.

--- StudentRecord.java
+++ StudentRecord.java
@@ -1,94 +1,24 @@
 /**
  * Student: Daniel Smith
  * Chapter 7
- * Programming Challenge 14: Phone Book ArrayList (altered to suit homework assignment, "HW Student Record")
- * The StudentRecord class stores a person's name, phone number, student ID and major.
- * 
- * The method names, class names and file names in this file and the demo file have been refactored, 
- * in order to draw contrast between it and the original Phone Book program.
+ * Programming Challenge 14: Student Record Class
+ * The StudentRecord class stores a person's name, phone number, student ID, and major.
  */
-
-public class StudentRecord
-{
-   private String name, phoneNumber, studentID, major; // Student's information.
+public class StudentRecord {
+   private String name, phoneNumber, studentID, major;
 
    /**
-    * The constructor initializes the person's name
-    * and phone number.
+    * Constructor to initialize the person's information.
     */
-   public StudentRecord(String name, String phoneNumber, String studentID, String major)
-   {
+   public StudentRecord(String name, String phoneNumber, String studentID, String major) {
       this.name = name;
       this.phoneNumber = phoneNumber;
-      this.studentID = studentID; 
-      this.major = major;
-   }
-   
-   /**
-    * The setName method sets the person's name.
-    */
-    public void setName(String name)
-    {
-       this.name = name;
-    }
-      
-   /**
-    * The setPhoneNumber method sets the person's 
-    * phone number.
-    */
-   public void setPhoneNumber(String phoneNumber)
-   {
-      this.phoneNumber = phoneNumber;
-   }
-
-   /**
-    * The setMajor method sets the person's 
-    * major.
-    */
-   public void setMajor(String major) {
-      this.major = major;
-   }
-   
-   /**
-    * The setStudentID method sets the person's 
-    * student ID.
-    */
-   public void setStudentID(String studentID) {
       this.studentID = studentID;
+      this.major = major;
    }
 
-   /**
-    * The The getName method returns the person's 
-    * name.
-    */
-   public String getName()
-   {
-      return name;
+   @Override
+   public String toString() {
+      return "Name: " + name + "\nPhone number: " + phoneNumber + "\nStudent ID: " + studentID + "\nMajor: " + major;
    }
-   
-   /**
-    * The getPhoneNumber method returns the
-    * person's phone number.
-    */
-   public String getPhoneNumber()
-   {
-      return phoneNumber;
-   }
-
-   /**
-    * The getStudentID method returns the person's 
-    * studentID.
-    */
-   public String getStudentID() {
-      return studentID;
-   }
-
-   /**
-    * The getMajor method returns the person's 
-    * major.
-    */
-   public String getMajor() {
-      return major;
-   }
-
 }
